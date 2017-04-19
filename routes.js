77936591abf1bedd034de35455d93ce40d9c1d45
Martin Kennedy <kennedym103@gmail.com>
2017-04-19 19:17:41 -0400

    index.js (#9)

--- routes.js
+++ routes.js
@@ -1,121 +1,24 @@
+const express = require('express');
+const router = express.Router();
 
-/* ^^^ EQUIVALENT TO:
-	const fsPromise = require('./fs-promise');
-	const fileRead = fsPromise.fileRead;
-*/
+const storage = require('./firebase.js');
 
-const onRequest = (request, response) => {
-	// console.log(request.url, request.method);
-	// ^^ same as...
-	let {url, method} = request;
-	const API_PREFIX = '/api';
+const parser = require('body-parser');
 
-	if (url.indexOf(API_PREFIX) === 0) {
-		const path = url.slice(API_PREFIX.length);
+router.use(parser.json());
 
-		// : move this out into own function eventually
-		if (path === '/' && method.toUpperCase() === 'GET') {
-			response.setHeader('Content-Type', 'application/json');
-			response.end(JSON.stringify(db.get('cardInfo').value()));
-		}
-		if (path === '/' && method.toUpperCase() === 'POST') {
-			// response.setHeader('Content-Type', 'application/json');
-			// response.end(JSON.stringify({data: []}));
-			const body = [];
-			request
+router.get('/login', (request, response) => {
 
-				.on('data', (chunk) => {
-					console.log(chunk)
-					body.push(chunk);
-				})
-				.on('end', () => {
-					const requestBody = Buffer.concat(body).toString();
-					// Add a post
-					db.get('cardInfo').push({
-						id: Date.now(), 
-						data: JSON.parse(requestBody)
-					}).write();
+     next();
+});
 
-					response.setHeader('Content-Type', 'application/json');
-					response.end(JSON.stringify(db.get('cardInfo').value()));
+router.post('/login', (request, response) => {
+     
+});
 
-				});
-
-			console.log('look ma! im in a post')
-		}
-		const lastBit = path.split('/').pop();
-		const isLastBitNum = !isNaN(lastBit);
-		const id = parseInt(lastBit, 10);
-
-		if (path.indexOf('/') === 0 && isLastBitNum && method.toUpperCase() === 'PUT') {
-			const body = [];
-			request
-				.on('data', (chunk) => {
-					console.log(chunk)
-					body.push(chunk);
-				})
-				.on('end', () => {
-					const requestBody = Buffer.concat(body).toString();
-					const dataPayload = JSON.parse(requestBody);
-					
-
-					db.get('cardInfo')
-					  .find({ id })
-					  .set('data.isDone', dataPayload.isDone)
-					  // .assign({ isDone: dataPayload.isDone })
-					  .write()
-
-					// // Add a post
-					// db.get('cardInfo').push({
-					// 	id: Date.now(), 
-					// 	data: JSON.parse(requestBody)
-					// }).write();
-
-					response.setHeader('Content-Type', 'application/json');
-					response.end(JSON.stringify(db.get('cardInfo').value()));
-
-				});
-		}
-
-		if (path.indexOf('/') === 0 && isLastBitNum && method.toUpperCase() === 'DELETE') {
-			console.log('me');
-
-			db.get('cardInfo')
-			  .remove({ id })
-			  .write()
+router.post('/signup', (request, response) => {
+     
+});
 
 
-			response.setHeader('Content-Type', 'application/json');
-			response.end(JSON.stringify(db.get('cardInfo').value()));
-		}
-	}
-	else {
-		if (url === '/') {
-			url = '/index.html';
-		}
-
-		const pathToFile = './public' + url;
-		// const pathToFile = './public' + (url === '/' ? '/index.html' : url);
-		// ^^^ opposite approach, less verbose but harder to read
-
-		fileRead(pathToFile)
-			.then((data) => {
-				response.end(data);
-			})
-			.catch((e) => {
-				response.statusCode = 404;
-				response.end('Kitten not found...?')
-			});
-	}
-
-	
-}
-
-
-module.exports = onRequest;
-
-
-
-
-
-
+module.exports = router;
