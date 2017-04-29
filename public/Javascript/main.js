a37f8755082972eeea8457d26849cb874d1cfe94
lisa ewart <lnemedium@gmail.com>
2017-04-28 20:46:22 -0400

    firebase script and main.js

--- public/Javascript/main.js
+++ public/Javascript/main.js
@@ -134,10 +134,111 @@ const signupButton = document.querySelector('.js-button-signup');
             });
         });
     }
+ 
+ function profilePictures (){
+
+        const validate = () => {
+            throw new Error('This is a required arg');
+        }; // validate
+
+        const photo = document.querySelector('.js-photo');
+        const photoSelect = document.querySelector('.js-photoSelect');
+
+
+        const uploadFiles = (
+            fileSelectSel = validate(),
+            fileElemSel = validate(),
+            onFileChanged = validate()
+        ) => {
+            // select anchor tag and file input
+            const fileSelect = document.querySelector(fileSelectSel);
+            const fileElem = document.querySelector(fileElemSel);
+
+            if (fileSelect === null || fileElem === null) {
+                throw new Error('Required DOM elements not found by querySelector');
+            }
+
+
+            // click handler for fileElem
+            fileSelect.addEventListener('click', (e) => {
+                e.preventDefault();
+                fileElem && fileElem.click();
+            });
+
+            // change handler for fileSelect
+            fileElem.addEventListener('change', (e) => onFileChanged(e.target.files))
+        } // uploadFiles
+
+
+        // Initialize Firebase
+         // Initialize Firebase
+            var config = {
+            apiKey: "AIzaSyANkdf3KcNrJ2EDGqCLn1te46zsk4cn04g",
+            authDomain: "nycda-instagram-clone.firebaseapp.com",
+            databaseURL: "https://nycda-instagram-clone.firebaseio.com",
+             projectId: "nycda-instagram-clone",
+             storageBucket: "nycda-instagram-clone.appspot.com",
+         messagingSenderId: "956508152154"
+            };
+        firebase.initializeApp(config);
+        // Name of file storage ref "folder"
+        const FILE_STORAGE_REF = 'images';
+
+        // initialize firebase
+        firebase.initializeApp(config);
+        // Get a reference to the storage service, which is used to create references in your storage bucket
+        const storageRef = firebase.storage().ref().child(FILE_STORAGE_REF);
+
+        uploadFiles('.js-fileSelect', '.js-fileElem', (files) => {
+            if (!storageRef) {
+                throw new Error('Storage Ref not set!');
+            }
+            const fileUploads = Array.from(files).map((currFile) => {
+                // we store the name of the file as a storage ref
+                const fileRef = storageRef.child(currFile.name);
+                // we return a promise where we first "put" or upload the file
+                // and then once the upload is complete, we return promise with
+                // download URL string of the file we uploaded
+                return fileRef.put(currFile).then((snapshot) => snapshot.downloadURL);
+            });
+
+            Promise.all(fileUploads).then((items) => {
+                console.log(items);
+            });
+        }); // upload files
+
+    }
+
+    const createPost = () => {
+
+    	const postSummary = document.querySelector('.js-summary');
+    	console.log(typeof postSummary);
+		postSummary.setAttribute('disabled', 'disabled');
+		console.log('made it past setAttribute');
+            // POST('/api/' + userId + '/post', {
+            //     descr: postSummary.value,
+            //     image_url: imageURL
+            // }).then((data) => {
+            //     // console.log(data)
+            //     postSummary.removeAttribute('disabled');
+            //     postSummary.value = '';
+            // });
+
+
+    }
+		document.querySelector('.js-createPost').addEventListener('click', (e) => {
+            e.preventDefault();
+            createPost();
+        });
+
+
+
 
 
 
 })();
+
+
 
 
 
